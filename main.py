4b87ef380223091fde7617b60eae355812fc09b4
RSkooTy <127528782+RSkooTy@users.noreply.github.com>
2025-03-08 23:52:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,27 +4,7 @@ from settings import *
 import math
 
 if __name__ == "__main__":
-#    best_plant = genetic_algorithm()
-#
-#    yield_total, results = calculate_yield(
-#        days=DAYS,
-#        initial_biomass=SEED_MASS,
-#        leaf_angle=best_plant.traits[1],
-#        grain_params=(FLOWERING_START_DAY, CAPSULE_MASS, GRAIN_FILLING_DURATION)
-#    )
-#
-#    last_day = results[-1]
-#    day_num, stage, stem_height, leaf_size, grain_biomass, buds, flowers, capsules, total_biomass, yield_total = last_day
-#
-#    print("\nРЕЗУЛЬТАТЫ МОДЕЛИРОВАНИЯ:")
-#    print(f"Длина стебля: {stem_height:.1f} м")
-#    print(f"Площадь листьев: {leaf_size:.2f} м²")
-#    print(f"Количество бутонов: {buds}")
-#    print(f"Количество цветов: {flowers}")
-#    print(f"Количество коробочек: {capsules}")
-#    print(f"Общая биомасса: {total_biomass:.2f} г")
-#    print(f"Урожайность: {yield_total:.2f} г")
-#
+
     print("_____________________________________________")
 
     yield_total, results  = calculate_yield(
